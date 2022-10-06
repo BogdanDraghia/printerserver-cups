25e27d6ec91a9c2eb162325da2c9932fe72223ce
BogdanDraghia <bgd.draghia@gmail.com>
2022-10-06 21:48:38 +0000

    reconfigured typescript project, added structure

---
+++ dist/src/providers/express.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Express {
+}
+exports.default = new Express();
+//# sourceMappingURL=express.js.map
---
+++ src/providers/express.ts
@@ -0,0 +1,11 @@
+import * as express from 'express';
+
+
+class Express{
+    
+    public express: express.Application;
+}
+
+export default new Express();
+
+
